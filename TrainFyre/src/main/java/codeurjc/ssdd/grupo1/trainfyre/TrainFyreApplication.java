42f8c344976f6e671ca90ad2855848d593776c9a
Pablo Sainz López <50539327+Escipion48@users.noreply.github.com>
2026-02-08 19:37:38 +0100

    Project Structure
    Establishment of the project structure with the whole team

---
+++ TrainFyre/src/main/java/codeurjc/ssdd/grupo1/trainfyre/TrainFyreApplication.java
@@ -0,0 +1,13 @@
+package codeurjc.ssdd.grupo1.trainfyre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainFyreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainFyreApplication.class, args);
+    }
+
+}
