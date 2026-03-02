4d4dbaed81602700c857f9d7b62f6b2d69d260a0
Escipion48 <escipio480@gmail.com>
2026-03-02 20:44:38 +0100

    REFACTORING bettering code quality and appliying DRY principles

---
+++ TrainFyre/src/main/resources/js/pages/settings.js
@@ -0,0 +1,4 @@
+import {genericPartialLoader} from "../utils/genericPartialLoader.js";
+
+genericPartialLoader("../../templates/partials/_header.html", '_header');
+genericPartialLoader('../../templates/partials/_footer.html', '_footer');
