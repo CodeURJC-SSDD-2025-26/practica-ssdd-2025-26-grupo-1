1f9bbf9ce6135bd2cae79d855aecd7634b5439bd
Escipion48 <escipio480@gmail.com>
2026-03-02 17:43:17 +0100

    Refactoring use of ; to avoid problems and more code clarity

--- TrainFyre/src/main/resources/js/pages/incidencies.js
+++ TrainFyre/src/main/resources/js/pages/incidencies.js
@@ -1,7 +1,7 @@
 import {genericPartialLoader} from "../utils/genericPartialLoader.js";
 
-genericPartialLoader("../../templates/partials/_header.html", '_header')
-genericPartialLoader('../../templates/partials/_footer.html', '_footer')
-genericPartialLoader('../../templates/partials/_pie_graph.html', '_pie_graph')
-genericPartialLoader('../../templates/partials/_bar_graph.html', '_bar_graph')
+genericPartialLoader("../../templates/partials/_header.html", '_header');
+genericPartialLoader('../../templates/partials/_footer.html', '_footer');
+genericPartialLoader('../../templates/partials/_pie_graph.html', '_pie_graph');
+genericPartialLoader('../../templates/partials/_bar_graph.html', '_bar_graph');
 
