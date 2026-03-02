d0668d92b8cb919561a612be44b3342f02a6fc7c
Escipion48 <escipio480@gmail.com>
2026-03-02 20:38:13 +0100

    REFACTORING bettering code quality and appliying DRY principles

--- TrainFyre/src/main/resources/js/pages/incidencies.js
+++ TrainFyre/src/main/resources/js/pages/incidencies.js
@@ -4,4 +4,3 @@ genericPartialLoader("../../templates/partials/_header.html", '_header');
 genericPartialLoader('../../templates/partials/_footer.html', '_footer');
 genericPartialLoader('../../templates/partials/_pie_graph.html', '_pie_graph');
 genericPartialLoader('../../templates/partials/_bar_graph.html', '_bar_graph');
-
