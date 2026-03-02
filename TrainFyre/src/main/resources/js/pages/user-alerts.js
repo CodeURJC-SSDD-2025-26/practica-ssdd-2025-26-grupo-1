c8786c9c506e919e04907018612c4068d5f48a83
Escipion48 <escipio480@gmail.com>
2026-03-02 20:34:20 +0100

    REFACTORING bettering code quality and appliying DRY principles

---
+++ TrainFyre/src/main/resources/js/pages/user-alerts.js
@@ -0,0 +1,5 @@
+import {genericPartialLoader} from "../utils/genericPartialLoader.js";
+
+genericPartialLoader("../../templates/partials/_header.html", '_header');
+genericPartialLoader('../../templates/partials/_alert_table.html', '_alert_table');
+genericPartialLoader('../../templates/partials/_footer.html', '_footer');
