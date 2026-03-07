7dad65da01970cdcf9e5ba0d5826039fc2170bf1
Posiblemente yo <dmontesj999@gmail.com>
2026-03-07 12:49:52 +0000

    Included different js files for each admin table.

---
+++ TrainFyre/src/main/resources/js/pages/admin-panel-incidences.js
@@ -0,0 +1,6 @@
+import {genericPartialLoader} from "../utils/genericPartialLoader.js";
+
+genericPartialLoader('../../resources/templates/partials/_header.html', '_header');
+genericPartialLoader('../../resources/templates/partials/_subheader.html', '_subheader');
+genericPartialLoader('../../resources/templates/partials/_incidence_table.html', '_incidence_table');
+genericPartialLoader('../../resources/templates/partials/_footer.html', '_footer');
