9262cdd359747b6057cfdc0a56690fc9e48cde15
Escipion48 <escipio480@gmail.com>
2026-03-19 14:00:29 +0100

    Https implementation

--- backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/SecurityConfiguration.java
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package codeurjc.ssdd.grupo1.trainfyre;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +13,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        /*http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().permitAll()
@@ -21,6 +22,14 @@ public class SecurityConfiguration {
                 .headers(headers -> headers
                         .frameOptions(frame -> frame.sameOrigin())
                 );
+        return http.build();*/
+        http
+                .csrf(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated());
+
         return http.build();
     }
 }
