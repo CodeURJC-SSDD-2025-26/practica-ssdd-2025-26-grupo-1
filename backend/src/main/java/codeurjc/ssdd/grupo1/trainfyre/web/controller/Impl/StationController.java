c70754042c0e9e28a598c89eab0bff9d56950f1b
Escipion48 <escipio480@gmail.com>
2026-04-08 08:27:06 +0200

    Change of log, now using the Slf4j annotation for better code quality in StationController.java for better up to date example for other team members

--- backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/web/controller/Impl/StationController.java
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/web/controller/Impl/StationController.java
@@ -3,8 +3,7 @@ package codeurjc.ssdd.grupo1.trainfyre.web.controller.Impl;
 
 import codeurjc.ssdd.grupo1.trainfyre.service.StationService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
@@ -13,15 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Validated
 @RequiredArgsConstructor
+@Slf4j
 public class StationController {
 
-    private final Logger logger = LoggerFactory.getLogger(StationController.class);
     private final StationService stationService;
 
     @GetMapping(value = "/stations")
     public String getStations(Model model) {
 
-        logger.info("getStations");
+        log.info("getStations");
 
         model.addAttribute("title", "Estaciones");
         model.addAttribute("name", "Pablo");
