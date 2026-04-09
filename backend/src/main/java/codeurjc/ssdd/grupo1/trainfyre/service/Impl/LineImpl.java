6e095b4420c777393d58fd1398622b945b3fc05c
Weisheng Zheng <128396359+WeishengZheng@users.noreply.github.com>
2026-04-09 22:13:54 +0000

    Wrong name fix

--- backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/service/Impl/LineImpl.java
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/service/Impl/LineImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class LineLineImpl implements LineService {
+public class LineImpl implements LineService {
     private LineRepository repository;
     private LineMapper LineRestMapper;
 
