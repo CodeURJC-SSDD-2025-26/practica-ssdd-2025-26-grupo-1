7f2e0432ccfc802fbeb0babbd35d7b6c3c1d29eb
AGB <alexgogb@gmail.com>
2026-04-09 09:30:16 +0200

    Added IncidenceService

---
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/service/IncidenceService.java
@@ -0,0 +1,9 @@
+package codeurjc.ssdd.grupo1.trainfyre.service;
+
+import codeurjc.ssdd.grupo1.trainfyre.dto.IncidenceDTO;
+
+import java.util.List;
+
+public interface IncidenceService {
+    List<IncidenceDTO> getAllIncidences();
+}
