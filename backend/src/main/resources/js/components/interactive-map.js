0ba5ad4d2c0258ab9c667a553d209b0e65ebe091
AGB <alexgogb@gmail.com>
2026-04-09 09:04:34 +0200

    Modified interactive-map.js

--- backend/src/main/resources/js/components/interactive-map.js
+++ backend/src/main/resources/js/components/interactive-map.js
@@ -2,22 +2,28 @@ export function initInteractiveMap() {
     const linesButtons = document.querySelectorAll('[data-action="highlightLine"]');
     const lines = [...linesButtons].map(button => button.id);
     const allPaths = document.querySelectorAll(lines.map(id => String.raw`g[inkscape\:label="${id}"]`).join(', '));
-
-    let timeout;
+    let effect = null
 
     function highlightLine(line){
+        if (effect !== highlightLine) {
+            effect = highlightLine;
+            allPaths.forEach(path => {
+                path.style.opacity = 1;
 
-        clearTimeout(timeout);
+                const visitedLine = path.getAttribute('inkscape:label');
 
-        allPaths.forEach(path => {
-            path.style.opacity = 1;
-
-            const visitedLine = path.getAttribute('inkscape:label');
-
-            if(visitedLine !== line.currentTarget.id){
-                path.style.opacity = 0.2;
-            }
-        })
+                if(visitedLine !== line.currentTarget.id){
+                    path.style.opacity = 0.2;
+                }
+            })
+        } else {
+            effect = null;
+            allPaths.forEach(path => {
+                path.style.opacity = 1;
+            });
+        }
+            
+        
     }
 
     linesButtons.forEach(lineButton => {
