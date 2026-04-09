61f0182f6be6b34d90608466ad25d820d0562361
AGB <alexgogb@gmail.com>
2026-04-09 21:15:28 +0200

    Added DTO mapper implementations

---
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/mapper/Impl/IncidenceDTOToIncidence.java
@@ -0,0 +1,15 @@
+package codeurjc.ssdd.grupo1.trainfyre.mapper.Impl;
+
+import codeurjc.ssdd.grupo1.trainfyre.data.model.Incidence;
+import codeurjc.ssdd.grupo1.trainfyre.dto.IncidenceDTO;
+import codeurjc.ssdd.grupo1.trainfyre.mapper.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IncidenceDTOToIncidence implements Mapper<IncidenceDTO, Incidence> {
+    @Override
+    public Incidence map(IncidenceDTO input) {
+        return new ModelMapper().map(input, Incidence.class);
+    }
+}
---
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/mapper/Impl/IncidenceToIncidenceDTO.java
@@ -0,0 +1,15 @@
+package codeurjc.ssdd.grupo1.trainfyre.mapper.Impl;
+
+import codeurjc.ssdd.grupo1.trainfyre.data.model.Incidence;
+import codeurjc.ssdd.grupo1.trainfyre.dto.IncidenceDTO;
+import codeurjc.ssdd.grupo1.trainfyre.mapper.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IncidenceToIncidenceDTO implements Mapper<Incidence, IncidenceDTO> {
+    @Override
+    public IncidenceDTO map(Incidence input) {
+        return new ModelMapper().map(input, IncidenceDTO.class);
+    }
+}
