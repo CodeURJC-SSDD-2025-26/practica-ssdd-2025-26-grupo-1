1680b82da5f8d86e54578f33c5059d468a8a1d08
Posiblemente yo <dmontesj999@gmail.com>
2026-04-09 07:59:31 +0000

    Added AlertDTO.

---
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/dto/AlertDTO.java
@@ -0,0 +1,22 @@
+package codeurjc.ssdd.grupo1.trainfyre.dto;
+
+import codeurjc.ssdd.grupo1.trainfyre.data.model.Line;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record AlertDTO(
+
+        String id,
+
+        String title,
+
+        Line line,
+
+        LocalDate startDate,
+        
+        LocalDate endDate,
+
+        LocalTime startHour,
+
+        LocalTime endHour
+){}
