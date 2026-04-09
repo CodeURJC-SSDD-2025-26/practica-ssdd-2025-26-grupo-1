d5ab2315830326c9a428f043b8772adbb8d949d3
AGB <alexgogb@gmail.com>
2026-04-09 09:34:24 +0200

    Added IncidenceMapper

---
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/mapper/IncidenceMapper.java
@@ -0,0 +1,13 @@
+package codeurjc.ssdd.grupo1.trainfyre.mapper;
+
+import codeurjc.ssdd.grupo1.trainfyre.data.model.Incidence;
+import codeurjc.ssdd.grupo1.trainfyre.dto.IncidenceDTO;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface IncidenceMapper {
+    Incidence toIncidence(IncidenceDTO incidenceDTO);
+
+    IncidenceDTO toIncidenceDTO(Incidence incidence);
+}
