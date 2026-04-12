60d5e4eeeb3af2b3cd8812f989463ba4c65b30ee
Daniel Montes Jiménez <d.montes.2023@alumnos.urjc.es>
2026-04-13 01:40:30 +0200

    Added queries to the Alert for the table.

--- backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/data/repository/AlertRepository.java
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/data/repository/AlertRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import codeurjc.ssdd.grupo1.trainfyre.data.model.Alert;
+import codeurjc.ssdd.grupo1.trainfyre.data.model.AppUser;
 import codeurjc.ssdd.grupo1.trainfyre.data.model.Line;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,5 @@ public interface AlertRepository extends JpaRepository<Alert, Long> {
     Optional<Alert> findByLineAndStartDateAndEndDate(Line lines, String start, String end);
 
     Page<Alert> findAll(Pageable page);
+    List<Alert> findByUserOrderByLine(AppUser user);
 }
