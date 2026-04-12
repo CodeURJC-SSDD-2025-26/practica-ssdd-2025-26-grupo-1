66d10d79ae9a09941d9aa9bd9a80fe0b6f3cde65
Weisheng Zheng <128396359+WeishengZheng@users.noreply.github.com>
2026-04-12 19:06:49 +0000

    add admin panel lines page

--- backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/web/controller/Impl/LineController.java
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/web/controller/Impl/LineController.java
@@ -1,9 +1,15 @@
 package codeurjc.ssdd.grupo1.trainfyre.web.controller.Impl;
 
 
+import codeurjc.ssdd.grupo1.trainfyre.dto.LineDTO;
+import codeurjc.ssdd.grupo1.trainfyre.dto.Role;
 import codeurjc.ssdd.grupo1.trainfyre.service.LineService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
@@ -26,5 +32,18 @@ public class LineController{
         model.addAttribute("lines", lineService.getAllLines());
 
         return "lines";
+    }
+
+    @GetMapping(value = "/admin/admin_panel_lines")
+    public String showAdminPanel(Model model) {
+        log.info("Loading admin lines panel");
+
+        model.addAttribute("title", "Admin Lines Panel");
+        //model.addAttribute("roles", Role.values());
+
+        //List<LineDTO> lines = lineService.getAllLines();
+        //model.addAttribute("lines", lines);
+
+        return "admin_panel";
     }
 }
