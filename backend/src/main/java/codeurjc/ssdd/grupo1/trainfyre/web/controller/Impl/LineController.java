d0583af0883a959f5a63e2909e5cd7e9e3bafa4c
Weisheng Zheng <128396359+WeishengZheng@users.noreply.github.com>
2026-04-13 15:08:23 +0000

    code: refactoring line controller dupiclated code

--- backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/web/controller/Impl/LineController.java
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/web/controller/Impl/LineController.java
@@ -1,6 +1,5 @@
 package codeurjc.ssdd.grupo1.trainfyre.web.controller.Impl;
 
-import codeurjc.ssdd.grupo1.trainfyre.dto.UserInfoDTO;
 import codeurjc.ssdd.grupo1.trainfyre.service.LineService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,19 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 @Slf4j
 public class LineController{
-
     private final LineService lineService;
-
-    @GetMapping(value = "/lines")
-    public String getLines(Model model) {
-
-        log.info("getLines");
-
-        model.addAttribute("title", "Lineas");
-        model.addAttribute("lines", lineService.getAllLines());
-
-        return "lines";
-    }
 
     @GetMapping(value = "/admin/admin_panel_lines")
     public String showAdminPanel(Model model) {
@@ -56,15 +42,6 @@ public class LineController{
         @RequestParam String linename, 
         Model model
     ) {
-        model.addAttribute("title", "Admin Lines Panel");
-
-        List<Map<String, Object>> lines = lineService.getAllLines().stream()
-            .map(line -> Map.<String, Object>of(
-                "line", line
-            ))
-                .toList();
-        model.addAttribute("lines", lines);
-
         lineService.deleteLine(linename);
 
         return "redirect:/admin/admin_panel_lines";
@@ -77,17 +54,7 @@ public class LineController{
         @RequestParam String description,
         Model model
     ) {
-
         log.info("Admin Updating line {}, to {}", oldName, newName);
-
-        model.addAttribute("title", "Admin Lines Panel");
-
-        List<Map<String, Object>> lines = lineService.getAllLines().stream()
-            .map(line -> Map.<String, Object>of(
-                "line", line
-            ))
-                .toList();
-        model.addAttribute("lines", lines);
 
         lineService.updateLine(oldName, newName, description);
 
@@ -100,17 +67,7 @@ public class LineController{
         @RequestParam String newDescription,
         Model model
     ) {
-
         log.info("Admin add line {}", newName);
-
-        model.addAttribute("title", "Admin Lines Panel");
-
-        List<Map<String, Object>> lines = lineService.getAllLines().stream()
-            .map(line -> Map.<String, Object>of(
-                "line", line
-            ))
-                .toList();
-        model.addAttribute("lines", lines);
 
         lineService.addLine(newName, newDescription);
 
