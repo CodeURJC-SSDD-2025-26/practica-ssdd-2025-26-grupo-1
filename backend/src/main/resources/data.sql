58910ec2effbf74d7c4309f221cbcf9ad02aa2bc
Weisheng Zheng <128396359+WeishengZheng@users.noreply.github.com>
2026-04-13 20:05:56 +0000

    fix: id remove from the sql querries to avoid conflicts with id sequence from lines

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,16 +9,16 @@ INSERT INTO station (id, address, name) VALUES (8, 'Calle Marqués de Irún, 2, 
 INSERT INTO station (id, address, name) VALUES (9, 'Calle Alfonso XII, s/n, Málaga', 'Málaga María Zambrano');
 INSERT INTO station (id, address, name) VALUES (10, 'Rúa do Hórreo, 75, Santiago de Compostela', 'Santiago de Compostela');
 
-INSERT INTO line (id, name, description, color) VALUES (999, 'C-1', 'Muy bonita', '#04f2ee');
-INSERT INTO line (id, name, description, color) VALUES (998, 'C-2', 'Muy verde', '#088a08');
-INSERT INTO line (id, name, description, color) VALUES (997, 'C-3', 'Muy predecible', '#5d03ac');
-INSERT INTO line (id, name, description, color) VALUES (996, 'C-4', 'Muy rapida', '#1818e0');
-INSERT INTO line (id, name, description, color) VALUES (995, 'C-5', 'Muy puntual', '#ffff3c');
-INSERT INTO line (id, name, description, color) VALUES (994, 'C-7', 'Muy buena', '#ff2d2d');
-INSERT INTO line (id, name, description, color) VALUES (993, 'C-8', 'Muy tranquila', '#707070');
-INSERT INTO line (id, name, description, color) VALUES (992, 'C-9', 'Muy nueva', '#823f00');
-INSERT INTO line (id, name, description, color) VALUES (991, 'C-10', 'Muy transitada', '#a2ff00');
-INSERT INTO line (id, name, description, color) VALUES (990, 'CIVIS', 'Muy desconocida', '#ff5a9f');
+INSERT INTO line (name, description, color) VALUES ('C-1', 'Muy bonita', '#04f2ee');
+INSERT INTO line (name, description, color) VALUES ('C-2', 'Muy verde', '#088a08');
+INSERT INTO line (name, description, color) VALUES ('C-3', 'Muy predecible', '#5d03ac');
+INSERT INTO line (name, description, color) VALUES ('C-4', 'Muy rapida', '#1818e0');
+INSERT INTO line (name, description, color) VALUES ('C-5', 'Muy puntual', '#ffff3c');
+INSERT INTO line (name, description, color) VALUES ('C-7', 'Muy buena', '#ff2d2d');
+INSERT INTO line (name, description, color) VALUES ('C-8', 'Muy tranquila', '#707070');
+INSERT INTO line (name, description, color) VALUES ('C-9', 'Muy nueva', '#823f00');
+INSERT INTO line (name, description, color) VALUES ('C-10', 'Muy transitada', '#a2ff00');
+INSERT INTO line (name, description, color) VALUES ('CIVIS', 'Muy desconocida', '#ff5a9f');
 
 
 
