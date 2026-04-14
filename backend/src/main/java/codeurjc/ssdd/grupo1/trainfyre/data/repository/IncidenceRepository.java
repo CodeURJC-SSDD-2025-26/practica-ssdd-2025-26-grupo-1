53ca2ccc610ab6ddc2ab4e1cbd1a8b4c567044a7
AGB <alexgogb@gmail.com>
2026-04-14 20:44:06 +0200

    Added findByAffectedLines method

--- backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/data/repository/IncidenceRepository.java
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/data/repository/IncidenceRepository.java
@@ -2,10 +2,13 @@ package codeurjc.ssdd.grupo1.trainfyre.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import codeurjc.ssdd.grupo1.trainfyre.data.model.Incidence;
+import codeurjc.ssdd.grupo1.trainfyre.data.model.Line;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface IncidenceRepository extends JpaRepository<Incidence, Long> {
+    List<Incidence> findByAffectedLinesContaining(Line line);
 }
