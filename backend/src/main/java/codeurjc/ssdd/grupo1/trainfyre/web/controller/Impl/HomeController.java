04e51157f2a2fa02b9f6c7d36b98e280698d0c5e
Weisheng Zheng <128396359+WeishengZheng@users.noreply.github.com>
2026-04-14 17:07:06 +0000

    code: add incidences table to index

--- backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/web/controller/Impl/HomeController.java
+++ backend/src/main/java/codeurjc/ssdd/grupo1/trainfyre/web/controller/Impl/HomeController.java
@@ -2,9 +2,18 @@ package codeurjc.ssdd.grupo1.trainfyre.web.controller.Impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import codeurjc.ssdd.grupo1.trainfyre.dto.IncidencesDTOs.IncidenceDTO;
+import codeurjc.ssdd.grupo1.trainfyre.service.IncidenceService;
+
 import org.springframework.ui.Model;
 
 @Controller
@@ -12,12 +21,22 @@ import org.springframework.ui.Model;
 @RequiredArgsConstructor
 @Slf4j
 public class HomeController {
+    @Autowired
+    private IncidenceService incidenceService;
+
     @GetMapping({"/", "index"})
     public String index(Model model) {
         log.info("Loading index page");
 
         model.addAttribute("title", "Index");
         model.addAttribute("interactiveMapScript", "pages/index.js");
+        try {
+            List<IncidenceDTO> incidenceDTOs = incidenceService.getAllIncidences();
+            model.addAttribute("incidences", incidenceDTOs);
+        } catch (Exception e) {
+            log.error("Error loading incidences: {}", e.getMessage());
+            model.addAttribute("incidences", Collections.emptyList());
+        }
 
         return "index";
     }
