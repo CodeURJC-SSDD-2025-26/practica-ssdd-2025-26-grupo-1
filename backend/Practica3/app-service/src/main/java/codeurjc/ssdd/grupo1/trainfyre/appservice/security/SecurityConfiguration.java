5dfd909ea582cced54baf1fd9f4c6fa69e7cc883
AGB <alexgogb@gmail.com>
2026-05-06 14:45:28 +0200

    Add security for incidencies

--- backend/Practica3/app-service/src/main/java/codeurjc/ssdd/grupo1/trainfyre/appservice/security/SecurityConfiguration.java
+++ backend/Practica3/app-service/src/main/java/codeurjc/ssdd/grupo1/trainfyre/appservice/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -63,6 +64,10 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/login", "/api/auth/refresh").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/v1/incidencies").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.PUT,"/v1/incidencies/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE,"/v1/incidencies/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/v1/incidencies/**").hasRole(Role.REGISTERED.name())
                         .anyRequest().authenticated()
                 );
 
